873534a33a6e22536eca1126fb500c4c72a9f096
Joe <kavvy99@naver.com>
2025-01-13 20:43:40 +0900

    [feat] 🐿️ MicroMiniBtn 컴포넌트 생성

---
+++ life_on_hana/components/atoms/MicroMiniBtn.tsx
@@ -0,0 +1,9 @@
+export default function MicroMiniBtn() {
+  return (
+    <>
+      <div className="flex justify-center items-center border border-hanapurple bg-hanalightpurple rounded-xl w-[2rem] h-[1.25rem] text-xs">
+        5
+      </div>
+    </>
+  );
+}
