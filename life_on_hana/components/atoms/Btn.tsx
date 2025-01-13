57f5ff610f3ba0e99a983a484cd2d44418caae0f
Joe <kavvy99@naver.com>
2025-01-13 23:37:21 +0900

    [fix] 🐿️ Btn 컴포넌트 내 variant 추가

--- life_on_hana/components/atoms/Btn.tsx
+++ life_on_hana/components/atoms/Btn.tsx
@@ -3,22 +3,27 @@ import Link from "next/link";
 type TBtnProps = {
   text: string;
   url?: string;
-  variant?: "default" | "link" | "beforeChooseAccount";
+  variant?: "default" | "moveToArticle" | "beforeChooseAccount" | "hanaWallet";
 };
 
 const getBtnClasses = (variant: string) => {
   switch (variant) {
+    // 하나지갑 페이지 안 버튼들
+    case "hanaWallet":
+      return "bg-hanalightpurple w-[330px] h-[47px] text-hanapurple text-[16px] font-SCDream8";
+    // 채우기 전
     case "beforeChooseAccount":
-      return "bg-hanagray rounded-xl w-[290px] h-[44px] text-white text-[15px] shadow-xl font-SCDream5";
-    case "link":
-      return "rounded-xl w-[307px] h-[47px] bg-hanapurple text-white text-[20px] font-bold font-SCDream5";
+      return "bg-hanagray w-[290px] h-[44px] text-[15px] shadow-xl";
+    // 전문보기
+    case "moveToArticle":
+      return "w-[307px] h-[47px] bg-hanapurple text-[20px] font-bold";
     default:
-      return "bg-hanapurple rounded-xl w-[290px] h-[44px] text-white text-[15px] font-SCDream5";
+      return "bg-hanapurple w-[290px] h-[44px] text-[15px]";
   }
 };
 
 export default function Btn({ text, url, variant = "default" }: TBtnProps) {
-  const btnClasses = getBtnClasses(variant);
+  const btnClasses = `${getBtnClasses(variant)} rounded-xl text-white font-SCDream5`;
   return url ? (
     <Link href={url}>
       <button className={btnClasses}>{text}</button>
