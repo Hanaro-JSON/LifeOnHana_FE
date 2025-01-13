5771e02469cea3a297a70d196b480a740c081d4d
Joe <kavvy99@naver.com>
2025-01-13 22:52:38 +0900

    [feat] 🐿️ Btn 컴포넌트 생성

---
+++ life_on_hana/components/atoms/Btn.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+type TBtnProps = {
+  text: string;
+  url?: string;
+  variant?: "default" | "link" | "beforeChooseAccount";
+};
+const getBtnClasses = (variant: string) => {
+  switch (variant) {
+    case "beforeChooseAccount":
+      return "bg-hanagray rounded-xl w-[290px] h-[44px] text-white text-[15px] shadow-xl font-SCDream5";
+    case "link":
+      return "rounded-xl w-[307px] h-[47px] bg-hanapurple text-white text-[20px] font-bold font-SCDream5";
+    default:
+      return "bg-hanapurple rounded-xl w-[290px] h-[44px] text-white text-[15px] font-SCDream5";
+  }
+};
+
+export default function Btn({ text, url, variant = "default" }: TBtnProps) {
+  const btnClasses = getBtnClasses(variant);
+  return url ? (
+    <Link href={url}>
+      <button className={btnClasses}>{text}</button>
+    </Link>
+  ) : (
+    <button className={btnClasses}>{text}</button>
+  );
+}
