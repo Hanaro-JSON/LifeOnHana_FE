3a395d4bc2b9fff4b7454774934b5c45781ab02f
Joe <kavvy99@naver.com>
2025-01-13 20:46:13 +0900

    [feat] 🐿️ MicroMiniBtn 컴포넌트 생성

--- life_on_hana/components/atoms/MicroMiniBtn.tsx
+++ life_on_hana/components/atoms/MicroMiniBtn.tsx
@@ -1,8 +1,8 @@
-export default function MicroMiniBtn() {
+export default function MicroMiniBtn({ num }: { num: number }) {
   return (
     <>
       <div className="flex justify-center items-center border border-hanapurple bg-hanalightpurple rounded-xl w-[2rem] h-[1.25rem] text-xs">
-        5
+        {num}
       </div>
     </>
   );
