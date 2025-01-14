e23b3e0d23073c9f4e45f15b7abfc468d2de68d8
jangdayeon <hyejmh@naver.com>
2025-01-14 15:38:59 +0900

    [docs] 🐳 CopyClipboardBtn storybook 작성 완료

---
+++ life_on_hana/stories/CopyClipboardBtn.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from "@storybook/react";
+import CopyClipboardBtn from "../components/atoms/CopyClipboardBtn";
+
+const meta: Meta<typeof CopyClipboardBtn> = {
+  title: "atom component/CopyClipboardBtn",
+  component: CopyClipboardBtn,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof CopyClipboardBtn>;
+
+export const Default: Story = {};
