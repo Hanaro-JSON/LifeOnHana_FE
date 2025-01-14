ca632b243fc02378fe7eb0ca3e5aed977e9cbbe3
Hana_JaeJun <wowns0912@naver.com>
2025-01-14 17:04:57 +0900

    [feat] 🐣 ShortsAutoToggle 추가

---
+++ life_on_hana/components/atoms/ShortsAutoToggle.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import React, { useState } from "react";
+import play from "../../assets/play.svg";
+import pause from "../../assets/pause.svg";
+
+interface TShortsAutoToggleProps {
+    initialState: "play" | "pause";
+    onToggle?: (state: "play" | "pause") => void;
+}
+
+export default function ShortsAutoToggle({
+    initialState = "play",
+    onToggle,
+}: TShortsAutoToggleProps) {
+    const [selected, setSelected] = useState<"play" | "pause">(initialState);
+
+    const handleToggle = (newState: "play" | "pause") => {
+    if (newState !== selected) {
+        setSelected(newState);
+        onToggle?.(newState);
+        }
+    };
+
+    return (
+    <div className="w-[1.8125rem] h-[.9375rem] absolute cursor-pointer select-none">
+        <div className="w-[1.8125rem] h-[.9375rem] left-0 top-0 absolute rounded-[.9375rem] border border-hanapurple" />
+
+        <div
+        className={`z-1 w-[.9375rem] h-[.9375rem] relative top-0 bg-hanapurple rounded-[.9375rem] border border-hanapurple transition-all duration-300 ${
+            selected === "play" ? "left-0" : "left-[.875rem]"
+        }`}
+        />
+
+        <div
+        className={`absolute left-[.38rem] top-[50%] transform -translate-y-1/2 text-center text-[.625rem] transition-colors duration-300`}
+        onClick={() => handleToggle("play")}
+        >
+        <Image
+            className={`transition-opacity duration-300 ${selected === "play" ? "opacity-100" : "opacity-0"} bg-hanapurple`}
+            src={play}
+            alt="Play"
+        />
+        </div>
+
+        <div
+        className={`absolute left-[1.19rem] top-[50%] transform -translate-y-1/2 text-center text-[.625rem] transition-colors duration-300`}
+        onClick={() => handleToggle("pause")}
+        >
+        <Image
+            className={`transition-opacity duration-300 ${selected === "pause" ? "opacity-100" : "opacity-0"} bg-hanapurple`}
+            src={pause}
+            alt="Pause"
+        />
+        </div>
+    </div>
+    );
+}
---
+++ life_on_hana/stories/ShortsAutoToggle.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ShortsAutoToggle from "../components/atoms/ShortsAutoToggle";
+
+const meta: Meta<typeof ShortsAutoToggle> = {
+  title: "Atom Component/ShortsAutoToggle",
+  component: ShortsAutoToggle,
+  tags: ["autodocs"],
+  argTypes: {
+    initialState: {
+      control: {
+        type: "radio",
+        options: ["play", "pause"],
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof ShortsAutoToggle>;
+
+export const Default: Story = {
+  args: {
+    initialState: "play",
+  },
+};
+
+export const Play: Story = {
+  args: {
+    initialState: "play",
+  },
+};
+
+export const Pause: Story = {
+  args: {
+    initialState: "pause",
+  },
+};
