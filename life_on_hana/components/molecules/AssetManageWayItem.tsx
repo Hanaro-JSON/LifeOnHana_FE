c91d3604cfb6a9ea05154be7ff43a4077867732e
Joe <kavvy99@naver.com>
2025-01-14 21:54:18 +0900

    [test] 🐿️ AssetManageWayItem 컴포넌트의 variant 타입 정의 및 타입 추가

--- life_on_hana/components/molecules/AssetManageWayItem.tsx
+++ life_on_hana/components/molecules/AssetManageWayItem.tsx
@@ -1,52 +1,89 @@
 import Image from "next/image";
 import Link from "next/link";
 
-// type TAssetManageWayItemProps = {
-//   variant?: 'adjust' | 'rebalancing' | 'managing' | 'product';
-// };
+type TAssetManageWayItemProps =
+  | "adjust"
+  | "rebalancing"
+  | "managing"
+  | "product"
+  | "invest"
+  | "trip"
+  | "culture"
+  | "realEstate";
 
-const getSrc = (variant: string) => {
+const getSrc = (variant: TAssetManageWayItemProps) => {
   switch (variant) {
-    // '지금 금액 조정'
     case "adjust":
       return "/assets/adjust.svg";
-    // '자산 리밸런싱'
     case "rebalancing":
       return "/assets/rebalancing.svg";
-    // '고정 지출 관리'
     case "managing":
       return "/assets/managing.svg";
-    // '대출 상품'
     case "product":
       return "/assets/product.svg";
+    case "invest":
+      return "/assets/invest.svg";
+    case "trip":
+      return "/assets/trip.svg";
+    case "culture":
+      return "/assets/culture.svg";
+    case "realEstate":
+      return "/assets/realEstate.svg";
     default:
       return "";
   }
 };
 
-const getLabel = (variant: string) => {
+const getLabel = (variant: TAssetManageWayItemProps) => {
   switch (variant) {
-    // '지금 금액 조정'
     case "adjust":
       return "지급 금액 조정";
-    // '자산 리밸런싱'
     case "rebalancing":
       return "자산 리밸런싱";
-    // '고정 지출 관리'
     case "managing":
       return "고정 지출 관리";
-    // '대출 상품'
     case "product":
       return "대출 상품";
+    case "invest":
+      return "투자";
+    case "trip":
+      return "여행";
+    case "culture":
+      return "문화";
+    case "realEstate":
+      return "부동산";
     default:
       return "";
   }
 };
 
-export default function AssetManageWayItem({ variant }: { variant: string }) {
+const getLink = (variant: TAssetManageWayItemProps) => {
+  switch (variant) {
+    case "adjust":
+      return "/";
+    case "rebalancing":
+      return "/";
+    case "managing":
+      return "/";
+    case "product":
+      return "/";
+    case "invest":
+      return "/";
+    case "trip":
+      return "/";
+    case "culture":
+      return "/";
+    case "realEstate":
+      return "/";
+    default:
+      return "/";
+  }
+};
+
+export default function AssetManageWayItem({ variant }: { variant: TAssetManageWayItemProps }) {
   return (
     <>
-      <Link href={"/"} className="flex flex-col items-center">
+      <Link href={getLink(variant)} className="flex flex-col items-center">
         <div className="w-10 h-10 bg-hanalightpurple rounded-xl flex justify-center items-center mb-1">
           <Image src={getSrc(variant)} alt={getLabel(variant)} width={30} height={30} />
         </div>
