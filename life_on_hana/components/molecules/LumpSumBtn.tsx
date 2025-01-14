34ad67222eb24a5b431df0d3f0c029899caab57f
Joe <kavvy99@naver.com>
2025-01-14 23:50:57 +0900

    [refactor] 🐿️ LumpSumBtn className 중복코드 제거

--- life_on_hana/components/molecules/LumpSumBtn.tsx
+++ life_on_hana/components/molecules/LumpSumBtn.tsx
@@ -36,29 +36,25 @@ export default function LumpSumBtn({ variant }: { variant: TLumpSumBtnProps }) {
 
   const clickEvent = () => setIsClicked(!isClicked);
 
+  const commonClassNames = "flex flex-col justify-center items-center w-[5.1875rem] h-[5.1875rem] rounded-xl";
+
   return (
     <>
-      {isClicked ? (
-        <button
-          onClick={clickEvent}
-          style={{
-            backgroundColor: "rgba(77, 0, 181, 0.2)",
-            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(77, 0, 181, 0.25) inset",
-          }}
-          className="flex flex-col justify-center items-center w-[5.1875rem] h-[5.1875rem] rounded-xl shadow-inner"
-        >
-          <Image src={getSrc(variant)} alt={getLabel(variant)} width={50} height={50} />
-          <div className="font-SCDream5 text-[.625rem]">{getLabel(variant)}</div>
-        </button>
-      ) : (
-        <button
-          onClick={clickEvent}
-          className="flex flex-col justify-center items-center w-[5.1875rem] h-[5.1875rem] bg-hanalightpurple rounded-xl shadow-xl"
-        >
-          <Image src={getSrc(variant)} alt={getLabel(variant)} width={50} height={50} />
-          <div className="font-SCDream5 text-[.625rem]">{getLabel(variant)}</div>
-        </button>
-      )}
+      <button
+        onClick={clickEvent}
+        style={
+          isClicked
+            ? {
+                backgroundColor: "rgba(77, 0, 181, 0.2)",
+                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(77, 0, 181, 0.25) inset",
+              }
+            : undefined
+        }
+        className={`${commonClassNames} ${isClicked ? "shadow-inner" : "bg-hanalightpurple"}`}
+      >
+        <Image src={getSrc(variant)} alt={getLabel(variant)} width={70} height={70} />
+        <div className="font-SCDream5 text-[.625rem]">{getLabel(variant)}</div>
+      </button>
     </>
   );
 }
