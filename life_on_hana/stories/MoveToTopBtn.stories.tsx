6127ac6d5662e00e303b00fba2a296b1439f703f
Joe <kavvy99@naver.com>
2025-01-14 15:08:10 +0900

    [test] 🐿️ MoveToTopBtn 컴포넌트 storybook 테스트 완료

---
+++ life_on_hana/stories/MoveToTopBtn.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from "@storybook/react";
+import MoveToTopBtn from "../components/atoms/MoveToTopBtn";
+
+const meta: Meta<typeof MoveToTopBtn> = {
+  title: "atom component/MoveToTopBtn",
+  component: MoveToTopBtn,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof MoveToTopBtn>;
+
+export const Default: Story = {
+  args: {},
+};
