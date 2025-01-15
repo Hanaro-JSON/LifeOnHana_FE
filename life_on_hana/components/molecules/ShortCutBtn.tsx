44fc20ddf1bd36dce85e05187cf45e7fe230e91c
Hana_JaeJun <wowns0912@naver.com>
2025-01-15 16:21:14 +0900

    [feat] 🐣 >를 Link에 추가

--- life_on_hana/components/molecules/ShortCutBtn.tsx
+++ life_on_hana/components/molecules/ShortCutBtn.tsx
@@ -34,15 +34,15 @@ export default function ShortCutBtn({ url, variant = "default" }: TShortCutBtnPr
   const { text, class1: variantClass1, class2: variantClass2 } = variantConfig[variant];
 
   return (
-    <div className={`${variantClass2}  h-[.9375rem] inline-flex items-center justify-center`}>
-      <Link href={url}>
+    <div className={`${variantClass2} h-[.9375rem] inline-flex items-center justify-center`}>
+      <Link href={url} className="flex items-center justify-center">
         <div className={`${variantClass1} h-3 text-right text-black font-light font-SCDream3 flex items-center justify-center`}>
           <span>{text}</span>
         </div>
+        <div className="w-[.9375rem] h-[.9375rem] relative flex items-center justify-center overflow-hidden">
+          <Image src={arrowRight} alt="Right Arrow" />
+        </div>
       </Link>
-      <div className="w-[.9375rem] h-[.9375rem] relative flex-col justify-start items-start flex overflow-hidden">
-        <Image src={arrowRight} alt="Right Arrow" />
-      </div>
     </div>
   );
 }
