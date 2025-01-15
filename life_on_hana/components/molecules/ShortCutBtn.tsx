cccdd62d3fd4b5602872fe73ce8685c3a07fbf0a
Hana_JaeJun <wowns0912@naver.com>
2025-01-15 15:37:20 +0900

    [feat] 🐣 ShortCutBtn 추가

---
+++ life_on_hana/components/molecules/ShortCutBtn.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import arrowRight from "../../assets/KeyboardArrowRight.svg";
+import Image from "next/image";
+
+type TShortCutBtnProps = {
+  url: string;
+  variant?: "default" | "column" | "spend" | "product";
+};
+
+const variantConfig = {
+  column: {
+    text: "좋아요한 칼럼 보러가기",
+    class1: "text-[.5rem] w-[5.25rem]",
+    class2: "w-[6.1875rem]",
+  },
+  spend: {
+    text: "내 지출내역 구체적으로 보기",
+    class1: "text-[.625rem] w-[8rem]",
+    class2: "w-[9rem]",
+  },
+  product: {
+    text: "관심 있을 상품 보러가기",
+    class1: "text-[.5rem] w-[5.25rem]",
+    class2: "w-[6.1875rem]",
+  },
+  default: {
+    text: "좋아요한 칼럼 보러가기",
+    class1: "text-[.5rem] w-[5.25rem]",
+    class2: "w-[6.1875rem]",
+  },
+};
+
+export default function ShortCutBtn({ url, variant = "default" }: TShortCutBtnProps) {
+  const { text, class1: variantClass1, class2: variantClass2 } = variantConfig[variant];
+
+  return (
+    <div className={`${variantClass2}  h-[.9375rem] inline-flex items-center justify-center`}>
+      <Link href={url}>
+        <div className={`${variantClass1} h-3 text-right text-black font-light font-SCDream3 flex items-center justify-center`}>
+          <span>{text}</span>
+        </div>
+      </Link>
+      <div className="w-[.9375rem] h-[.9375rem] relative flex-col justify-start items-start flex overflow-hidden">
+        <Image src={arrowRight} alt="Right Arrow" />
+      </div>
+    </div>
+  );
+}
---
+++ life_on_hana/stories/ShortCutBtn.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ShortCutBtn from "@/components/molecules/ShortCutBtn";
+
+const meta: Meta<typeof ShortCutBtn> = {
+  title: "molecule component/ShortCutBtn",
+  component: ShortCutBtn,
+  tags: ["autodocs"],
+  argTypes: {
+    url: { control: "text", description: "Link" },
+    variant: {
+      control: "radio",
+      options: ["default", "column", "spend", "product"],
+      description: "바로가기",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof ShortCutBtn>;
+
+export const Default: Story = {
+  args: {
+    url: "https://www.kebhana.com",
+    variant: "default",
+  },
+};
+
+export const Column: Story = {
+  args: {
+    url: "https://www.kebhana.com",
+    variant: "column",
+  },
+};
+
+export const Spend: Story = {
+  args: {
+    url: "https://www.kebhana.com",
+    variant: "spend",
+  },
+};
+
+export const Product: Story = {
+  args: {
+    url: "https://www.kebhana.com",
+    variant: "product",
+  },
+};
