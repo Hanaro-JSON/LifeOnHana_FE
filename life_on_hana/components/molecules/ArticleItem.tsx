b88a5567100c9b808bb87e5739368d7eb8cc7ed0
Hana_JaeJun <wowns0912@naver.com>
2025-01-15 16:27:10 +0900

    [feat] 🐣 ArticleItem 추가

---
+++ life_on_hana/components/molecules/ArticleItem.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import HeartNo from "../../assets/HeartNo.svg";
+import HeartYes from "../../assets/HeartYes.svg";
+
+type ArticleItemProps = {
+  title: string;
+  category: string;
+  published_at: string;
+  thumbnail_s3_key: string;
+  is_liked: boolean;
+};
+
+export default function ArticleItem({
+  title,
+  category,
+  published_at,
+  thumbnail_s3_key,
+  is_liked,
+}: ArticleItemProps) {
+  return (
+    <div className="w-[21.4375rem] h-[4.375rem] relative">
+      <div className="w-5 h-5 left-[19.625rem] top-[2.5rem] absolute" />
+      
+      <div className="w-52 left-[6.625rem] top-0 absolute text-black text-sm font-SCDream5">
+        {title}
+      </div>
+      
+      <Image
+        className="w-[4.75rem] h-[3.75rem] left-[.8125rem] top-0 absolute rounded-[.625rem]"
+        src={thumbnail_s3_key}
+        alt="Article Image"
+        width={76}
+        height={60}
+      />
+      
+      <div className="absolute left-[19.625rem] top-[2.5rem]">
+        <Image
+          src={is_liked ? HeartYes : HeartNo}
+          alt={is_liked ? "Liked" : "Not Liked"}
+          width={20} 
+          height={20}
+        />
+      </div>
+      
+      <div className="w-[21.4375rem] h-px left-0 top-[4.3125rem] absolute bg-[#d9d9d9]" />
+      
+      <div className="left-[6.625rem] top-[3rem] absolute text-black text-[.625rem] font-SCDream3">
+        {category} / {published_at}
+      </div>
+    </div>
+  );
+}
---
+++ life_on_hana/stories/ArticleItem.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ArticleItem from "@/components/molecules/ArticleItem";
+
+const meta: Meta<typeof ArticleItem> = {
+  title: "molecule component/ArticleItem",
+  component: ArticleItem,
+  tags: ["autodocs"],
+  argTypes: {
+    title: { control: "text", description: "제목" },
+    category: { control: "text", description: "카테고리" },
+    published_at: { control: "text", description: "날짜" },
+    thumbnail_s3_key: { control: "text", description: "썸네일 이미지 URL" },
+    is_liked: { control: "boolean", description: "좋아요 여부" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof ArticleItem>;
+
+export const Default: Story = {
+  args: {
+    title: "고요함과 활기참 그 사이 라오스의 두 가지 매력",
+    category: "여행",
+    published_at: "2024년 07월",
+    thumbnail_s3_key: "https://hana1qm.com/dataFile/bbs/202404240904500690.jpg",
+    is_liked: false,
+  },
+};
+
+export const Liked: Story = {
+  args: {
+    title: "중고거래, AI가 올려주는 당신의 매너온도",
+    category: "투자",
+    published_at: "2024년 12월",
+    thumbnail_s3_key: "https://www.hana1qm.com/dataFile/bbs/202420210420420822.jpg",
+    is_liked: true,
+  },
+};
