c33556f3773d9e75678dfabf7d0b49d7e436bc1a
Hana_JaeJun <wowns0912@naver.com>
2025-01-15 17:02:59 +0900

    [feat] 🐣 IsLike 추가

---
+++ life_on_hana/stories/IsLike.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from "@storybook/react";
+import IsLike from "@/components/molecules/IsLike";
+
+const meta: Meta<typeof IsLike> = {
+  title: "Molecule Component/IsLike",
+  component: IsLike,
+  tags: ["autodocs"],
+  argTypes: {
+    likeCount: { control: "number", description: "초기 좋아요 개수" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof IsLike>;
+
+export const Default: Story = {
+  args: {
+    likeCount: 10,
+    isLiked: false,
+  },
+};
+
+export const Liked: Story = {
+  args: {
+    likeCount: 100,
+    isLiked: true,
+  },
+};
+
+export const NoLikes: Story = {
+  args: {
+    likeCount: 0,
+  },
+};
