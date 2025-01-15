8c0c94b44c61f00a4628a84bf26811ffbdf0126d
Joe <kavvy99@naver.com>
2025-01-15 16:04:16 +0900

    [style] 🐿️ 화면 사이즈에 영향을 주는 불필요한 코드 삭제

--- life_on_hana/app/(routes)/whilick/page.tsx
+++ life_on_hana/app/(routes)/whilick/page.tsx
@@ -1,9 +1,7 @@
 export default function Whilick() {
   return (
     <>
-      <div className="relative h-full">
-        <div className="bg-hanagray w-[500px] h-[5000px]">Whilick</div>
-      </div>
+      <div className="relative h-full">Whilick</div>
     </>
   );
 }
--- life_on_hana/app/page.tsx
+++ life_on_hana/app/page.tsx
@@ -1,9 +1,7 @@
 export default function RootPage() {
   return (
     <>
-      <div className="relative h-full">
-        <div className="bg-hanagray w-[500px] h-[5000px]">RootPage</div>
-      </div>
+      <div className="relative h-full">RootPage</div>
     </>
   );
 }
