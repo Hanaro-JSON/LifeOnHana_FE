2ce991df14e8fc081d04f6c43cb809580b6f1c40
Hana_JaeJun <wowns0912@naver.com>
2025-01-15 17:48:42 +0900

    [style] 🐣 주석 수정

--- life_on_hana/stories/ConnectBankItem.stories.tsx
+++ life_on_hana/stories/ConnectBankItem.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 import ConnectBankItem from "@/components/molecules/ConnectBankItem";
 
-// Meta 정보 설정
 const meta: Meta<typeof ConnectBankItem> = {
   title: "molecule component/ConnectBankItem", 
   component: ConnectBankItem, 
