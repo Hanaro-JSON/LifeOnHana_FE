0d4d17282323902c54757bfb9a23a7139f01ba45
Joe <kavvy99@naver.com>
2025-01-17 00:14:48 +0900

    [style] 🐿️ px -> rem 단위 통일

--- life_on_hana/app/(routes)/signin/mydata/page.tsx
+++ life_on_hana/app/(routes)/signin/mydata/page.tsx
@@ -23,17 +23,17 @@ export default function Mydata() {
   return (
     <>
       <div className="relative h-full ">
-        <div className="pt-[5rem] px-[1.5rem] mb-[120px]">
-          <div className="font-SCDream3 text-[23px] mb-2">
+        <div className="pt-[5rem] px-[1.5rem] mb-[7.5rem]">
+          <div className="font-SCDream3 text-[1.6rem] mb-2">
             <span className="font-Hana2heavy text-hanapurple">LIFE on HANA</span> 가입을 위해
             <br />
             마이데이터 서비스를 <span className="font-SCDream5">가입</span>합니다
           </div>
-          <div className="font-SCDream3 text-[12px] text-hanadeepgray mb-20">
+          <div className="font-SCDream3 text-[.75rem] text-hanadeepgray mb-14">
             탈퇴 시 마이데이터로 연결된 모든 자산과 개인정보가 삭제(유효한 전송요구도 함께 중단)되며, 서비스 재이용을
             위해서는 다시 전송동의가 필요해요.{" "}
           </div>
-          <div className="flex items-center gap-3 font-SCDream5 text-[14px] mb-5">
+          <div className="flex items-center gap-3 font-SCDream5 text-[1.2rem] mb-5">
             연결되는 데이터
             <MicroMiniBtn num={clickedNum} />
           </div>
@@ -50,7 +50,7 @@ export default function Mydata() {
 
         <button
           onClick={moveToHomeEvent}
-          className="rounded-t-xl fixed w-full bottom-0 h-[80px] flex justify-center pt-5 font-SCDream3 text-[15px] bg-hanapurple text-white"
+          className="rounded-t-xl fixed w-full bottom-0 h-[6.5rem] flex justify-center pt-5 font-SCDream3 text-[1rem] bg-hanapurple text-white"
         >
           마이데이터 서비스 연결
         </button>
