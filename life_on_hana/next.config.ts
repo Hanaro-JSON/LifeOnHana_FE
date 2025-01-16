5aa6e7f63038eb39923e7a7a77620e3d8ac83883
Hana_JaeJun <wowns0912@naver.com>
2025-01-16 16:50:12 +0900

    [feat] 🐣 column 페이지 추가

--- life_on_hana/next.config.ts
+++ life_on_hana/next.config.ts
@@ -4,4 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// columnPage에서 하나원큐에서 사진을 불러오기 위한 연결용 - 목데이터 삭제시 삭제할것
+module.exports = {
+  images: {
+    domains: ['hana1qm.com'],
+  },
+};
+//
+
 export default nextConfig;
