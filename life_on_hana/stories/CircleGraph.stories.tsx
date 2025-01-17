967fe3c520f5994595ed930fdf651155ac809663
jangdayeon <hyejmh@naver.com>
2025-01-17 10:05:46 +0900

    [chore] 🐳 import에 type 명시

--- life_on_hana/stories/CircleGraph.stories.tsx
+++ life_on_hana/stories/CircleGraph.stories.tsx
@@ -1,10 +1,14 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { CircleGraph } from "@/components/molecules/CircleGraph";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { type TGraphExpenseCategoriesProps } from "@/types/componentTypes";
 =======
 import { TGraphExpenseCategoriesProps } from "@/types/componentTypes";
 >>>>>>> 4544294 ([docs] 🐳 CircleGraph storybook 작성 완료)
+=======
+import { type TGraphExpenseCategoriesProps } from "@/types/componentTypes";
+>>>>>>> 37b88d1 ([chore] 🐳 import에 type 명시)
 const meta: Meta<typeof CircleGraph> = {
   title: "Molecule Component/CircleGraph",
   component: CircleGraph,
