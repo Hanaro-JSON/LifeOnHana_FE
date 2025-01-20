93ac95d7a59fdc3cba7fe2dd00c2005b2803fa05
Joe <kavvy99@naver.com>
2025-01-20 23:44:03 +0900

    [chore] 🐿️ solve conflict in next.config.ts

--- life_on_hana/next.config.ts
+++ life_on_hana/next.config.ts
@@ -7,17 +7,7 @@ const nextConfig: NextConfig = {
 // columnPage에서 하나원큐에서 사진을 불러오기 위한 연결용 - 목데이터 삭제시 삭제할것
 module.exports = {
   images: {
-<<<<<<< HEAD
-<<<<<<< HEAD
     domains: ['www.hana1qm.com', 'hana1qm.com'],
-    
-=======
-    domains: ['www.hana1qm.com'],
->>>>>>> 4018117 ([fix] 🐣 columnDetail 페이지 수정)
-=======
-    domains: ['www.hana1qm.com', 'hana1qm.com'],
-    
->>>>>>> d257b51 ([fix] 🐣 column관련 목록과 자세히 보기 동적으로 수정)
   },
 };
 //
