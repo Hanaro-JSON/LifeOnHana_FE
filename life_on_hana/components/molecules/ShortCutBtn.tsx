ad83116573b21d750473ea73e9a384840e87e988
jangdayeon <hyejmh@naver.com>
2025-01-21 09:48:42 +0900

    [chore] 🐳 prettier 완료

--- life_on_hana/components/molecules/ShortCutBtn.tsx
+++ life_on_hana/components/molecules/ShortCutBtn.tsx
@@ -1,34 +1,34 @@
-import Link from "next/link";
-import arrowRight from "../../assets/KeyboardArrowRight.svg";
-import Image from "next/image";
-import { type TShortCutBtnProps } from "@/types/componentTypes";
+import Link from 'next/link';
+import arrowRight from '../../assets/KeyboardArrowRight.svg';
+import Image from 'next/image';
+import { type TShortCutBtnProps } from '@/types/componentTypes';
 
 const variantConfig = {
   column: {
-    text: "좋아요한 칼럼 보러가기",
-    class1: "text-[.75rem]",
-    class2: "",
+    text: '좋아요한 칼럼 보러가기',
+    class1: 'text-[.75rem]',
+    class2: '',
   },
   spend: {
-    text: "내 지출내역 구체적으로 보기",
-    class1: "text-[.75rem]",
-    class2: "",
+    text: '내 지출내역 구체적으로 보기',
+    class1: 'text-[.75rem]',
+    class2: '',
   },
   product: {
-    text: "관심 있을 상품 보러가기",
-    class1: "text-[.75rem]",
-    class2: "",
+    text: '관심 있을 상품 보러가기',
+    class1: 'text-[.75rem]',
+    class2: '',
   },
   default: {
-    text: "좋아요한 칼럼 보러가기",
-    class1: "text-[.75rem]",
-    class2: "",
+    text: '좋아요한 칼럼 보러가기',
+    class1: 'text-[.75rem]',
+    class2: '',
   },
 };
 
 export default function ShortCutBtn({
   url,
-  variant = "default",
+  variant = 'default',
 }: TShortCutBtnProps) {
   const {
     text,
@@ -40,14 +40,14 @@ export default function ShortCutBtn({
     <div
       className={`${variantClass2} h-[.9375rem] inline-flex items-center justify-center`}
     >
-      <Link href={url} className="flex items-center justify-center">
+      <Link href={url} className='flex items-center justify-center'>
         <div
           className={`${variantClass1} h-3 text-right text-black font-light font-SCDream3 flex items-center justify-center`}
         >
           <span>{text}</span>
         </div>
-        <div className="w-[.9375rem] h-[.9375rem] relative flex items-center justify-center overflow-hidden">
-          <Image src={arrowRight} alt="Right Arrow" />
+        <div className='w-[.9375rem] h-[.9375rem] relative flex items-center justify-center overflow-hidden'>
+          <Image src={arrowRight} alt='Right Arrow' />
         </div>
       </Link>
     </div>
--- life_on_hana/components/molecules/WhilickCarousel.tsx
+++ life_on_hana/components/molecules/WhilickCarousel.tsx
@@ -1,42 +1,38 @@
-import { Carousel } from "nuka-carousel";
-import SmallWhilickItem from "./SmallWhilickItem";
-import { TArticlesLiked } from "@/types/dataTypes";
-import Image from "next/image";
-import whilickIcon from "@/assets/whilickIcon.svg";
-export function WhilickCarousel({
-  items,
-}: {
-  items: TArticlesLiked[];
-}) {
-  function CarouselItem({
-    articleId,
-    title,
-    category,
-  }: TArticlesLiked) {
+import { Carousel } from 'nuka-carousel';
+import SmallWhilickItem from './SmallWhilickItem';
+import { TArticlesLiked } from '@/types/dataTypes';
+import Image from 'next/image';
+import whilickIcon from '@/assets/whilickIcon.svg';
+export function WhilickCarousel({ items }: { items: TArticlesLiked[] }) {
+  function CarouselItem({ articleId, title, category }: TArticlesLiked) {
     return (
-        <SmallWhilickItem article_id={articleId} title={title} variant={category}/>
+      <SmallWhilickItem
+        article_id={articleId}
+        title={title}
+        variant={category}
+      />
     );
   }
   return (
     <>
-      <div className="flex flex-row w-full gap-2 font-SCDream5 text-lg">
-        <Image src={whilickIcon} alt={"whilickIcon"}/>
+      <div className='flex flex-row w-full gap-2 font-SCDream5 text-lg'>
+        <Image src={whilickIcon} alt={'whilickIcon'} />
         휘릭으로 보기
       </div>
-    <Carousel
-      wrapMode="wrap"
-      className="relative w-full"
-      scrollDistance="slide"
-    >
-      {items.map((item) => (
-        <CarouselItem
-          key={item.articleId}
-          articleId={item.articleId}
-          title={item.title}
-          category={item.category}
-        />
-      ))}
+      <Carousel
+        wrapMode='wrap'
+        className='relative w-full'
+        scrollDistance='slide'
+      >
+        {items.map((item) => (
+          <CarouselItem
+            key={item.articleId}
+            articleId={item.articleId}
+            title={item.title}
+            category={item.category}
+          />
+        ))}
       </Carousel>
-      </>
+    </>
   );
 }
