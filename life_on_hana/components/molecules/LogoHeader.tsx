bc11802c99cfb206f41094e08267351ec8ede60e
Joe <kavvy99@naver.com>
2025-01-22 16:23:29 +0900

    [feat] 🐿️ next-auth의 로그아웃 처리

--- life_on_hana/components/molecules/LogoHeader.tsx
+++ life_on_hana/components/molecules/LogoHeader.tsx
@@ -1,15 +1,21 @@
 import Image from 'next/image';
 import signoutbtn from '@/assets/signoutBtn.svg';
+import { mySignOut } from '@/actions/myauth';
+
 export function LogoHeader({ isMain }: { isMain: boolean }) {
+  const handleSignout = () => {
+    mySignOut();
+  };
+
   return (
     <div className='flex flex-row justify-between items-center'>
       <div className='w-full flex items-center font-Hana2heavy text-[1.5625rem] text-hanapurple'>
         LIFE on HANA
       </div>
-      {isMain === true ? (
-        <Image src={signoutbtn} alt='signoutbtn' width={18} height={18} />
-      ) : (
-        <></>
+      {isMain === true && (
+        <button onClick={handleSignout}>
+          <Image src={signoutbtn} alt='signoutbtn' width={18} height={18} />
+        </button>
       )}
     </div>
   );
