d1a59f5f3fc331b61c653240d19cdfe7025b7a92
Joe <kavvy99@naver.com>
2025-01-24 18:37:53 +0900

    [feat] 🐿️ SmallWhilickItem 통해 설정된 localStorage의 article_id 존재 유무에 따라 달라지는 api 처리

--- life_on_hana/app/(routes)/whilick/page.tsx
+++ life_on_hana/app/(routes)/whilick/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import Image from 'next/image';
@@ -7,26 +8,27 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import WhilickItemLoading from '@/components/molecules/WhilickItemLoading';
 import useDebounce from '@/hooks/useDebounce';
 import { getApiToken, NEXT_PUBLIC_URL } from '@/api';
-// import { fetchWhilicks } from '@/api';
+import { type TWhilickContents } from '@/types/dataTypes';
 
-type TWhilickContents = {
-  articleId: number;
-  title: string;
-  isLiked: boolean;
-  likeCount: number;
-  text: {
-    paragraphId: number;
-    content: string;
-    startTime: number;
-    endTime: number;
-  }[];
-  ttsUrl: string;
+type TWhilickData = {
+  contents: TWhilickContents[];
+  pageable: {
+    first: boolean;
+    last: boolean;
+    pageNumber: number;
+    pageSize: number;
+    totalElements: number;
+    totalPages: number;
+  };
 };
 
 export default function Whilick() {
+  // home/columns > WhilickCarousel > SmallWhilickItem 통해 설정된 localStorage의 article_id 값
+  localStorage.setItem('article_id', '20');
+
   const [globalAudioState, setGlobalAudioState] = useState({
-    isPlaying: true,
-    isMute: false,
+    isPlaying: false,
+    isMute: true,
   });
   const [globalAudioSpeed, setGlobalAudioSpeed] = useState(1.0);
   const [globalFontSize, setGlobalFontSize] = useState(1.0);
@@ -41,44 +43,63 @@ export default function Whilick() {
     }
   }, []);
 
+  const whilickItemTop = Math.floor(debouncedTop / window.innerHeight);
+
   // ----------------------- api 통신 --------------------------------
+
   const [fetchData, setFetchData] = useState([]);
-  const [whilickData, setWhilickData] = useState([]);
+  const [whilickData, setWhilickData] = useState<TWhilickData>();
   const [whilickContents, setWhilickContens] = useState<TWhilickContents[]>([]);
 
   const token = getApiToken();
 
+  const articleIdData = localStorage.getItem('article_id');
+
+  // useEffect(() => {
+  //   if (fetchData) {
+  //     console.log('whilickData>>>', whilickData);
+  //     console.log('whilickContents>>>', whilickContents);
+  //     console.log('휘릭아이템 탑:>>>', whilickItemTop);
+  //   }
+  // }, [fetchData, whilickData, whilickContents, whilickItemTop]);
+
   useEffect(() => {
-    const fetchWhilickList = async () => {
+    const fetchWhilickList = async (page = 0) => {
+      const getChangableApi = (page: number) => {
+        if (articleIdData) {
+          const articleId = JSON.parse(articleIdData);
+          if (articleId) {
+            return `/api/articles/shorts/${articleId}`;
+          } else {
+            return `/api/articles/shorts?page=${page}&size=10`;
+          }
+        }
+      };
+      const apiUrl = `${process.env.NEXT_PUBLIC_URL}${getChangableApi(page)}`;
+      console.log('apiUrl: ', apiUrl);
       try {
         let currentToken = token;
 
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_URL}/api/articles/shorts?page=0&size=10`,
-          {
+        const response = await fetch(`${apiUrl}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${currentToken}`,
+            credentials: 'include',
+          },
+        });
+
+        // 토큰 갱신 실패
+        if (response.status === 401) {
+          currentToken = await refreshToken();
+          const retryResponse = await fetch(`${apiUrl}`, {
             method: 'GET',
             headers: {
               'Content-Type': 'application/json',
               Authorization: `Bearer ${currentToken}`,
               credentials: 'include',
             },
-          }
-        );
-
-        // 토큰 갱신 실패
-        if (response.status === 401) {
-          currentToken = await refreshToken();
-          const retryResponse = await fetch(
-            `${NEXT_PUBLIC_URL}/api/articles/shorts?page=0&size=10`,
-            {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${currentToken}`,
-                credentials: 'include',
-              },
-            }
-          );
+          });
           if (!retryResponse.ok)
             throw new Error(`Error: ${retryResponse.status}`);
           const data = await retryResponse.json();
@@ -92,14 +113,30 @@ export default function Whilick() {
           setFetchData(data);
           setWhilickData(data.data);
           setWhilickContens(data.data.contents);
+
+          // articleIdData가 있었을 경우, fetch 후에 해당 데이터 삭제
+          if (articleIdData) {
+            localStorage.removeItem('article_id');
+          }
         }
       } catch (error) {
-        console.error('Failed to fetch whilick list:', error);
+        console.error('휘릭 불러오기 실패', error);
       }
     };
 
     fetchWhilickList();
-  }, [token]);
+
+    if (whilickItemTop === 5 && !whilickData?.pageable.last) {
+      const nextPage = (whilickData?.pageable.pageNumber ?? 0) + 1;
+      fetchWhilickList(nextPage);
+    }
+  }, [
+    token,
+    whilickItemTop,
+    whilickData?.pageable.last,
+    whilickData?.pageable.pageNumber,
+    articleIdData,
+  ]);
 
   // 토큰 갱신
   const refreshToken = async () => {
@@ -116,13 +153,6 @@ export default function Whilick() {
       throw error;
     }
   };
-
-  useEffect(() => {
-    if (fetchData) {
-      console.log('whilickData>>>', whilickData);
-      console.log('whilickContens>>>', whilickContents);
-    }
-  }, [fetchData, whilickData, whilickContents]);
 
   return (
     <>
