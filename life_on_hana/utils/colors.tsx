c1bea4ee2887fcb4deb4b4d6c75e35e90878b444
jangdayeon <dayeonjang.dev@gmail.com>
2025-01-28 23:43:31 +0900

    [refactor] 🐳 그래프 색상 변경

--- life_on_hana/utils/colors.tsx
+++ life_on_hana/utils/colors.tsx
@@ -1,14 +1,20 @@
+// export const bluePurpleColors: string[] = [
+//   '#FFB6D9',
+//   '#D67BFF',
+//   '#793FDF',
+//   '#798BFF',
+//   '#5FBDFF',
+//   '#96EFFF',
+//   '#2E7A69',
+//   '#77E4C8',
+//   '#C1FBA4',
+//   '#FEFFAC',
+// ];
 export const bluePurpleColors: string[] = [
-  // '#F1F3F4', // Deep Navy
-  '#79BAC1', // Steel Blue
-  '#2A7886', // Cool Indigo
-  '#512B58', // Slate Blue
-  '#f4ebfb',
-  '#4d00b5',
-  '#6C22A6', // Light Navy
-  '#6962AD', // Midnight Blue
-  '#83C0C1', // Dusty Blue
-  // '#5D7081', // Shadow Blue
-  // '#6A7D89', // Faded Navy
-  // '#4E6D92', // Cool Azure
+  '#4609AE',
+  '#936ECD',
+  '#0C0D0E',
+  '#575759',
+  '#23045C',
+  '#E9E1F4',
 ];
