ad509a84702bdddf831d674c6777796f38e22f41
jangdayeon <hyejmh@naver.com>
2025-02-03 15:25:25 +0900

    [fix] 🐳 signout redirect 문제 해결

--- life_on_hana/middleware.ts
+++ life_on_hana/middleware.ts
@@ -3,14 +3,13 @@ import { auth } from './lib/auth';
 export async function middleware(req: NextRequest) {
   const session = await auth();
   const didLogin = !!session?.user;
-  if (!didLogin) {
-    // const callbackUrl = encodeURIComponent(req.nextUrl.pathname);
+  if (!didLogin && req.nextUrl.pathname !== '/') {
     return NextResponse.redirect(new URL(`/`, req.url));
   }
   return NextResponse.next();
 }
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|robots.txt|images|api/auth|signin|$).*)',
+    '/((?!^$|^/$|_next/static|_next/image|favicon.ico|robots.txt|images|api/auth|signin|columns/.*).*)',
   ],
 };
