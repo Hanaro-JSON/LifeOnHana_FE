54e329267b33031085843d92cc803503c8948f60
Joe <kavvy99@naver.com>
2025-02-04 13:41:51 +0900

    [fix] 🐿️ Whilick 페이지 내 데이터 fetch 함수 및 무한스크롤 로직 수정

--- life_on_hana/app/(routes)/whilick/page.tsx
+++ life_on_hana/app/(routes)/whilick/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 
 import Image from 'next/image';
@@ -6,10 +7,12 @@ import WhilickItem from '@/components/molecules/WhilickItem';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import WhilickItemLoading from '@/components/molecules/WhilickItemLoading';
 import useDebounce from '@/hooks/useDebounce';
-import { type TWhilickData, type TWhilickContents } from '@/types/dataTypes';
-import { getApiToken } from '@/api';
+import { type TWhilickData } from '@/types/dataTypes';
+import { fetchWhilickList } from '@/api';
+import { useToast } from '@/hooks/use-toast';
 
 export default function Whilick() {
+  const { toast } = useToast();
   const [globalAudioState, setGlobalAudioState] = useState({
     isPlaying: false,
     isMute: true,
@@ -19,7 +22,7 @@ export default function Whilick() {
 
   const [top, setTop] = useState(0);
   const scrollRef = useRef<HTMLDivElement>(null);
-  const debouncedTop = useDebounce(top, 50);
+  const debouncedTop = useDebounce(top, 20);
 
   const handleScroll = useCallback(() => {
     if (scrollRef.current) {
@@ -29,128 +32,60 @@ export default function Whilick() {
 
   // ----------------------- api 통신 --------------------------------
 
-  const [whilickData, setWhilickData] = useState<TWhilickData>();
-  const [wholeData, setWholeData] = useState<TWhilickContents[]>([]);
-  const [viewings, setViewings] = useState<TWhilickContents[]>([]);
+  const [whilickData, setWhilickData] = useState<TWhilickData | null>(null);
+  const articleIdData = localStorage.getItem('article_id');
   const [currentPage, setCurrentPage] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [isLast, setIsLast] = useState(false);
 
-  const articleIdData = localStorage.getItem('article_id');
-
-  const getChangableApi = useCallback(
-    (page: number) => {
-      if (articleIdData) {
-        const articleId = JSON.parse(articleIdData);
-        return `/api/articles/shorts/${articleId}`;
-      } else {
-        return `/api/articles/shorts?page=${page}&size=10`;
-      }
-    },
-    [articleIdData]
-  );
-
-  const fetchData = useCallback(
-    async (page: number = 0) => {
-      setIsLoading(true);
-      try {
-        const currentToken = getApiToken();
-        const apiUrl = `${process.env.NEXT_PUBLIC_URL}${getChangableApi(page)}`;
-
-        console.log('Fetching:', apiUrl);
-
-        const response = await fetch(apiUrl, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${currentToken}`,
-            credentials: 'include',
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
-
-        const data = await response.json();
-        setWhilickData(data.data);
-        setWholeData((prev) => [...prev, ...data.data.contents]);
-
-        // articleIdData가 있었다면, fetch 후에 삭제
-        if (articleIdData) {
-          localStorage.removeItem('article_id');
-        }
-
-        if (data.data.pageable.last === true) {
-          setIsLast(true);
-        }
-
-        setCurrentPage((prev) => prev + 1);
-      } catch (error) {
-        console.error('휘릭 불러오기 실패', error);
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    [articleIdData, getChangableApi]
-  );
-
-  // 첫 데이터 fetch
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
-  // 첫 5개 항목을 viewings에 할당
-  useEffect(() => {
-    if (wholeData.length > 0) {
-      setViewings(wholeData.slice(0, 5));
-    }
-  }, [wholeData]);
-
-  // whilickItemTop을 통해 현재 스크롤 위치 계산
   const whilickItemTop = Math.floor(debouncedTop / window.innerHeight);
 
-  // 스크롤이 마지막 항목에 근접하면 데이터를 추가로 로딩할 시점
   useEffect(() => {
-    if (whilickItemTop >= wholeData.length / 5 - 1 && !isLoading && !isLast) {
-      fetchData(currentPage);
+    console.log('whilickItemTop', whilickItemTop);
+  }, [whilickItemTop]);
+
+  useEffect(() => {
+    console.log('whilickData: ', whilickData);
+  }, [whilickData]);
+
+  // ---------------------------- first fetch ----------------------------
+  useEffect(() => {
+    fetchWhilickList(currentPage, articleIdData, setWhilickData);
+  }, [articleIdData]);
+
+  // ---------------------------- more fetch ----------------------------
+  const loadMoreData = useCallback(() => {
+    setIsLoading(true);
+    fetchWhilickList(currentPage + 1, articleIdData, setWhilickData).finally(
+      () => {
+        setIsLoading(false);
+        setCurrentPage((prev) => prev + 1);
+        setIsLast(whilickData?.pageable.last ?? false);
+      }
+    );
+  }, [currentPage, articleIdData, setWhilickData]);
+
+  useEffect(() => {
+    if (
+      whilickItemTop > 0 &&
+      whilickItemTop % 7 === 0 &&
+      !isLoading &&
+      !isLast &&
+      currentPage < 10
+    ) {
+      loadMoreData();
     }
-  }, [whilickItemTop, wholeData, currentPage, isLoading, isLast, fetchData]);
+  }, [whilickItemTop]);
 
-  // 현재 보고 있는 항목을 기준으로 시작과 끝 범위 설정하는 것으로 viewings 배열 동적 수정
   useEffect(() => {
-    if (wholeData.length > 0) {
-      const currentIndex = Math.floor(whilickItemTop * 5); // 현재 보고 있는 항목의 인덱스
-      const startIndex = Math.max(0, currentIndex - 2);
-      const endIndex = Math.min(wholeData.length, currentIndex + 3);
-
-      // viewings 배열을 갱신하기 전에 이전 데이터를 기준으로 덧붙여나가는 방식
-      setViewings((prevViewings) => {
-        const newViewings = wholeData.slice(startIndex, endIndex);
-
-        // 이미 추가된 viewings와 겹치는 부분을 제외하고, 새로운 데이터만 추가
-        const updatedViewings = [
-          ...prevViewings.filter(
-            (item) =>
-              !newViewings.some(
-                (newItem) => newItem.articleId === item.articleId
-              )
-          ),
-          ...newViewings,
-        ];
-
-        return updatedViewings;
+    if (whilickItemTop === 103) {
+      toast({
+        title: '더이상 콘텐츠가 없습니다.',
+        className:
+          'flex justify-center fixed top-[80%] left-[50%] transform -translate-x-[50%] bg-white text-hanapurple w-[90%] text-center rounded-xl p-7',
       });
     }
-  }, [whilickItemTop, wholeData]);
-
-  // useEffect(() => {
-  //   console.log('wholeData: ', wholeData);
-  //   console.log('currentPage: ', currentPage);
-  //   console.log('viewings: ', viewings);
-  //   console.log('isLast: ', isLast);
-  //   console.log('whilickItemTop: ', whilickItemTop);
-  // }, [currentPage, viewings, wholeData, isLast, whilickItemTop]);
+  });
 
   return (
     <>
@@ -181,11 +116,11 @@ export default function Whilick() {
               msOverflowStyle: 'none',
             }}
           >
-            {viewings?.map(
+            {whilickData?.contents?.map(
               ({ articleId, title, text, isLiked, likeCount, ttsUrl }, idx) => (
                 <WhilickItem
                   idx={idx}
-                  key={articleId}
+                  key={idx}
                   title={title}
                   text={text}
                   articleId={articleId}
